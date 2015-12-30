59815ed59dae79fb16f45bf024b1cfa3f3c3155f
Timothy Horng <thorng@asu.edu>
2015-12-30 14:36:08 -0700

    Initial Commit

---
+++ Tips CodePath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tips CodePath
+//
+//  Created by Timothy Horng on 12/30/15.
+//  Copyright © 2015 Timothy Horng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
